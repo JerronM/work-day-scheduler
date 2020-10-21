c201448e5befec370f46a871ddc611f4cc421ec9
Jerron <jerron.mcginnis@gmail.com>
2020-10-21 17:37:38 -0400

    Created content for giving and checking current time and saving notes to local storage

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+
+//Provides current time and checks the current time to determine which CSS rule to remove/apply to the time box which correlates with the current time
+$(document).ready(function () {
+  setInterval(function () {
+      var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+      $("#currentDay").text(currentTime);
+      for (var i = 9; i < 18; i++) {
+          if (i === moment().hour()) {
+              $("#hour-" + i).removeClass("past present future").addClass("present");
+          } else if (i < moment().hour()) {
+              $("#hour-" + i).removeClass("past present future").addClass("past");
+          } else {
+              $("#hour-" + i).removeClass("past present future").addClass("future");
+          }
+      }
+  }, 1000);
+  //User clicks button to save text in time block to local stoage
+  var timeboxText;
+  $(".saveBtn").on("click", function () {
+      timeboxText = $(this).siblings("textarea")
+      var userInput = timeboxText.val();
+      var timeBlock = timeboxText.attr("id");
+      localStorage.setItem(timeBlock, userInput);
+  });
+  function getLocalStorage() {
+      for (var i = 9; i < 18; i++) {
+          $("#hour-" + i).val(localStorage.getItem("hour-" + i));
+      }
+  }
+  getLocalStorage();
+});
